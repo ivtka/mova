770da6b6dfc01f4b07d60c8c1209d29062d07764
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-10 20:57:45 +0000

    Add views for language test

--- language_tests/views.py
+++ language_tests/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpRequest
+from django.views import View, CreateView, DeleteView, ListView, DetailView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import forms, models
+
+
+class HomeView(View):
+    def get(self, request: HttpRequest):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('redirection')
+        return render(request, '')  # TODO: write html template
+
+
+class DashboardView(View):
+    def get(self, request: HttpRequest):
+        if request.user.is_superuser:
+            return redirect('admin-dashboard')
+        return redirect('user-dashboard')
+
+
+class AdminMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class AdminDashboardView(AdminMixin, View):
+    def get(self, request: HttpRequest):
+        return render(request, '')  # TODO: write html template
+
+
+class AdminAddLanguageView(AdminMixin, CreateView):
+    model = models.Language
+    form_class = forms.LanguageForm
+    template_name = ''  # TODO: write html template for form
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class AdminLanguagesView(AdminMixin, ListView):
+    model = models.Language
+    template_name = ''  # TODO: write html template for list
+
+
+class AdminLanguageView(AdminMixin, DetailView):
+    model = models.Language
+    template_name = ''  # TODO: write html template for language
+
+
+class AdminDeleteLanguageView(AdminMixin, DeleteView):
+    model = models.Language
+    success_url = 'dashboard'
+
+class AdminAddQuestionView(AdminMixin, CreateView):
+    model = models.Question
+    form_class = forms.QuestionForm
+    template_name = ''  # TODO: write html template for add question
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.language_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+
+class AdminQuestionsView(AdminMixin, ListView):
+    model = models.Question
+    template_name = ''  # TODO: write html template
+
+
+class AdminDeleteQuestionView(AdminMixin, DelteView):
+    model = models.Question
+    success_url = reverse_lazy('dashboard')
