be7ff83e19df503f9f106a5f1f6670b7b9e5f273
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-10 21:10:24 +0000

    Add user app

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'language_tests'
+    'language_tests',
+    'users'
 ]
 
 MIDDLEWARE = [
