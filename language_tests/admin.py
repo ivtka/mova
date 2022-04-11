6abda1b5f536933a128624d663743fd6a68fb993
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-11 00:31:36 +0000

    Register models

--- language_tests/admin.py
+++ language_tests/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from language_tests.models import Language, Level, Question
+
+admin.site.register(Language)
+admin.site.register(Level)
+admin.site.register(Question)
