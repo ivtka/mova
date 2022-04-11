9a3407a8de0623838d1a5146aa6d1aa1342deb3d
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-11 03:26:49 +0000

    Add extra urls

--- language_tests/urls.py
+++ language_tests/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from language_tests.views import HomeView, DashboardView
+from language_tests.views import (AdminAddLanguageView, AdminAddQuestionView,
+                                  AdminDashboardView, DashboardView, HomeView)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('dashboard', DashboardView.as_view(), name='dashboard')
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
+    path('admin-dashboard', AdminDashboardView.as_view(), name='admin-dashboard'),
+    path('admin-dashboard/add-language',
+         AdminAddLanguageView.as_view(), name='add-language'),
+    path('admin-dashboard/add-question',
+         AdminAddQuestionView.as_view(), name='add-question')
 ]
