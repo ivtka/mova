a9ea83c21fabb05566f29d9c72985d97ffbac282
Ivan Tkachuk <94393106+ivtkac@users.noreply.github.com>
2022-04-12 02:28:53 +0300

    Add language test Add template and view for language test. Update correct urls. Add simple calculate of level.

--- language_tests/models.py
+++ language_tests/models.py
@@ -6,15 +6,22 @@ from django.contrib.auth.models import User
 class Language(models.Model):
     language_name = models.CharField(max_length=50, unique=True)
     max_questions = models.PositiveIntegerField()
+    time = models.PositiveIntegerField(help_text="Тривалість тесту")
 
     def __str__(self) -> str:
         return self.language_name
+
+    def get_questions(self):
+        return self.question_set.all()
 
 
 class Level(models.Model):
     LEVELS = (('A1', 'A1'), ('A2', 'A2'), ('B1', 'B1'),
               ('B2', 'B2'), ('C1', 'C1'), ('C2', 'C2'))
     level = models.CharField(unique=True, max_length=2, choices=LEVELS)
+
+    def __str__(self) -> str:
+        return self.level
 
 
 class Question(models.Model):
@@ -28,6 +35,9 @@ class Question(models.Model):
     OPTIONS = (('Option1', 'Option1'), ('Option2', 'Option2'),
                ('Option3', 'Option3'), ('Option4', 'Option4'))
     answer = models.CharField(max_length=200, choices=OPTIONS)
+
+    def get_level(self):
+        return self.level
 
 
 class Result(models.Model):
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from users.views import StartTestView, TakeTestView, UserDashboardView, UserRegisterView, LanguageView
+from users.views import (LanguageView, ResultView, UserDashboardView, UserRegisterView,
+                         start_test, calculate_level_view)
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('dashboard/', UserDashboardView.as_view(), name='user-dashboard'),
     path('dashboard/language/<int:pk>', LanguageView.as_view(), name='language'),
-    path('dashboard/language/<int:pk>/start-test',
-         StartTestView.as_view(), name='start-test'),
-    path('dashboard/language/<int:pk>/take-test',
-         TakeTestView.as_view(), name='take-test')
+    path('dashboard/language/start-test/<int:pk>',
+         start_test, name='start_test'),
+    path('calculate-level', calculate_level_view, name="calculate-view"),
+    path('view-result', ResultView.as_view(), name='view-result')
 ]
