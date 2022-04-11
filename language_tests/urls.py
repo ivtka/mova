0bfe19e323dc39c5dd63ad68fad024d4d922ca7b
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-11 05:49:43 +0000

    Update

--- language_tests/urls.py
+++ language_tests/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from language_tests.views import (AdminAddLanguageView, AdminAddQuestionView,
-                                  AdminDashboardView, DashboardView, HomeView)
+                                  AdminDashboardView, DashboardView, HomeView, )
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin-dashboard/add-language',
          AdminAddLanguageView.as_view(), name='add-language'),
     path('admin-dashboard/add-question',
-         AdminAddQuestionView.as_view(), name='add-question')
+         AdminAddQuestionView.as_view(), name='add-question'),
+     path('languages/', )
 ]
