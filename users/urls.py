bee292688cf20bfa56edfef27edb0681216f74bc
Ivan Tkachuk <94393106+ivtkac@users.noreply.github.com>
2022-04-18 21:33:16 +0300

    Remove email confirmation

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,11 @@ from users.views import (LanguageView, ResultView, UserDashboardView, UserRegist
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', activate_view, name='activate'),
     path('dashboard/', UserDashboardView.as_view(), name='user-dashboard'),
-    path('dashboard/language/<int:pk>', LanguageView.as_view(), name='language'),
-    path('dashboard/language/start-test/<int:pk>',
+    path('dashboard/language/<int:pk>/', LanguageView.as_view(), name='language'),
+    path('dashboard/language/start-test/<int:pk>/',
          start_test, name='start_test'),
-    path('calculate-level', calculate_level_view, name="calculate-view"),
-    path('view-result', ResultView.as_view(), name='view-result')
+    path('calculate-level/', calculate_level_view, name="calculate-view"),
+    path('view-result/', ResultView.as_view(), name='view-result')
 ]
